5505f4085bc5185221db02478ebca92a57023ac0
Adarsh Kumar Singh <145589188+Adarsh0710@users.noreply.github.com>
2026-04-29 20:16:15 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,10 +7,10 @@ export default defineConfig({
     // port: 5173,
     port: 'https://shree-ganesh-angency.onrender.com',
     proxy: {
-      // '/api': { target: 'http://localhost:5000', changeOrigin: true },
-      // '/uploads': { target: 'http://localhost:5000', changeOrigin: true },
-      '/api': { target: 'https://shree-ganesh-angency.onrender.com', changeOrigin: true },
-      '/uploads': { target: 'https://shree-ganesh-angency.onrender.com', changeOrigin: true },
+      '/api': { target: 'http://localhost:5000', changeOrigin: true },
+      '/uploads': { target: 'http://localhost:5000', changeOrigin: true },
+      // '/api': { target: 'https://shree-ganesh-angency.onrender.com', changeOrigin: true },
+      // '/uploads': { target: 'https://shree-ganesh-angency.onrender.com', changeOrigin: true },
     },
   },
 });
