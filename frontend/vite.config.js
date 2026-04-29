d3ff7b45dbc65a1b4652e196f4473a1b8c8e3542
Adarsh Kumar Singh <145589188+Adarsh0710@users.noreply.github.com>
2026-04-29 20:04:55 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,10 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/api': { target: 'http://localhost:5000', changeOrigin: true },
-      '/uploads': { target: 'http://localhost:5000', changeOrigin: true },
+      // '/api': { target: 'http://localhost:5000', changeOrigin: true },
+      // '/uploads': { target: 'http://localhost:5000', changeOrigin: true },
+      '/api': { target: 'https://shree-ganesh-angency.onrender.com', changeOrigin: true },
+      '/uploads': { target: 'https://shree-ganesh-angency.onrender.com', changeOrigin: true },
     },
   },
 });
