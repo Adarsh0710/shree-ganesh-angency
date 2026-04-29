bc97b46b6a24fc648e0dd125b9632010404c3991
Adarsh Kumar Singh <145589188+Adarsh0710@users.noreply.github.com>
2026-04-29 20:21:43 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    // port: 5173,
-    port: 'https://shree-ganesh-angency.onrender.com',
+    port: 5173,
+    // port: 'https://shree-ganesh-angency.onrender.com',
     proxy: {
-      '/api': { target: 'http://localhost:5000', changeOrigin: true },
-      '/uploads': { target: 'http://localhost:5000', changeOrigin: true },
-      // '/api': { target: 'https://shree-ganesh-angency.onrender.com', changeOrigin: true },
-      // '/uploads': { target: 'https://shree-ganesh-angency.onrender.com', changeOrigin: true },
+      // '/api': { target: 'http://localhost:5000', changeOrigin: true },
+      // '/uploads': { target: 'http://localhost:5000', changeOrigin: true },
+      '/api': { target: 'https://shree-ganesh-angency.onrender.com', changeOrigin: true },
+      '/uploads': { target: 'https://shree-ganesh-angency.onrender.com', changeOrigin: true },
     },
   },
 });
