ee857b44385fe3bb6958f7ba134d15978ca4b182
Adarsh Kumar Singh <145589188+Adarsh0710@users.noreply.github.com>
2026-04-29 20:13:07 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    // port: 5173,
+    port: 'https://shree-ganesh-angency.onrender.com',
     proxy: {
       // '/api': { target: 'http://localhost:5000', changeOrigin: true },
       // '/uploads': { target: 'http://localhost:5000', changeOrigin: true },
